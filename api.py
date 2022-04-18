44d14b491cb35a0155266bb846993c6a0ffa4918
kolamax25 <75153987+kolamax25@users.noreply.github.com>
2022-04-17 22:15:06 -0700

    Add files via upload

---
+++ api.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify, request
+import csv
+
+all_articles = []
+
+with open('shared_articles.csv', encoding = 'utf-8') as f:
+    data = list(csv.reader(f))
+    all_articles = data[1:]
+
+liked_articles = []
+not_liked_articles = []
+
+app = Flask(__name__)
+
+@app.route("/get-article")
+def get_articles():
+    return jsonify({
+        "data": all_articles[0],
+        "status": "success"
+    })
+
+@app.route("/liked-articles", methods=["POST"])
+def liked_articles():
+    article = all_articles[0]
+    all_articles = all_articles[1:]
+    liked_articles.append(article)
+    return jsonify({
+        "status": "success"
+    }), 201
+
+@app.route("/unliked-article", methods=["POST"])
+def unliked_article():
+    article = all_articles[0]
+    all_articles = all_articles[1:]
+    not_liked_articles.append(article)
+    return jsonify({
+        "status": "success"
+    }), 201
+
+if __name__ == "__main__":
+  app.run()
